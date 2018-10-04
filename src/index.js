66251f0a37612a31e1b778341c61c2960137f216
selfimper <mr.tennant@outlook.com>
2018-10-04 22:06:32 +0800

    props

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Welcome from './Welcome'
 
 ReactDOM.render(
-    <h1>Hi, world!</h1>,
+    // <h1>Hi, world!</h1>,
+    <Welcome name="Tennant"/>,
     document.getElementById('root2')
   );
