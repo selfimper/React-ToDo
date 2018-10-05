68fefd7ec04a6c0bfe20489af50f5f7eaf9c7a97
selfimper <mr.tennant@outlook.com>
2018-10-05 09:11:06 +0800

    删除Welcome

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Welcome from './Welcome'
+
 
 ReactDOM.render(
-    // <h1>Hi, world!</h1>,
-    <Welcome name="Tennant"/>,
+    <App />,
     document.getElementById('root2')
   );
