ca6bf663f14fb21584473258b31da1dc68d51b28
selfimper <mr.tennant@outlook.com>
2018-10-05 16:09:10 +0800

    标记为已完成/未完成

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
     let todos = this.state.todoList.map((item,index)=>{
       return (
         <li key="{index}">
-          <TodoItem todo={item} />
+          <TodoItem todo={item} onToggle={this.toggle.bind(this)}/>
         </li>
       )
     })
@@ -38,6 +38,10 @@ class App extends Component {
         </ol>
       </div>
     )
+  }
+  toggle(e, todo){
+    todo.status = todo.status === 'completed' ? '' : 'completed'
+    this.setState(this.state) 
   }
   changeTitle(event){
     this.setState({
