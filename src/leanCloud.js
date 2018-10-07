459eba80d008f3f0bb62b98bfb7cadf897d8b7db
selfimper <mr.tennant@outlook.com>
2018-10-07 10:45:04 +0800

    添加返回登录按钮

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -48,12 +48,12 @@ AV.init({
     AV.User.logOut()
     return undefined
   }
-  
+
   export function sendPasswordResetEmail(email, successFn, errorFn){
     AV.User.requestPasswordReset(email).then(function (success) {
       successFn.call() 
     }, function (error) {
-      console.dir(error)
+      errorFn.call(null, error)
     })
   }
 
