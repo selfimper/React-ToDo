2f54dc7dd8327991fc2de989cc344b85a1bdaae0
selfimper <mr.tennant@outlook.com>
2018-10-07 12:51:14 +0800

    一个组件有方法，也可以改写成一个函数

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './TodoInput.css'
 
- export default class TodoInput extends Component {
-  render(){
-    return <input type="text" value={this.props.content}
-      className="TodoInput"
-      onChange={this.changeTitle.bind(this)}
-      onKeyPress={this.submit.bind(this)}/>
-  }
-  submit(e){
-    if (e.key === 'Enter') {
-      this.props.onSubmit(e)
-    }
-  }
-  changeTitle(e){
-    this.props.onChange(e)
+function submit (props, e) {
+  if (e.key === 'Enter') {
+    props.onSubmit(e)
   }
 }
+function changeTitle (props, e) {
+  props.onChange(e)
+}
+
+//bind用法
+//https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+
+ export default function (props) {
+  return <input type="text" value={props.content}
+    className="TodoInput"
+    onChange={changeTitle.bind(null, props)}
+    onKeyPress={submit.bind(null, props)}/>
+  }
