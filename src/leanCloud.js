b29512bde81ef30d0ce90f67a8f6c97cd1a58e08
selfimper <mr.tennant@outlook.com>
2018-10-07 09:16:47 +0800

    仿照注册，完成登录功能

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -24,6 +24,15 @@ AV.init({
     return undefined
   }
 
+  export function signIn(username, password, successFn, errorFn){
+    AV.User.logIn(username, password).then(function (loginedUser) {
+      let user = getUserFromAVUser(loginedUser)
+      successFn.call(null, user)
+    }, function (error) {
+      errorFn.call(null, error)
+    })
+  }
+
   export function getCurrentUser(){
     let user = AV.User.current()
     if(user){
