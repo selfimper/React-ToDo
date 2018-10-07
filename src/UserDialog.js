de44494fc37f3424475936e95d4ab27e53761ba8
selfimper <mr.tennant@outlook.com>
2018-10-07 10:50:05 +0800

    将 SignInForm 抽离成一个组件

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './UserDialog.css';
 import {signUp, signIn, sendPasswordResetEmail} from './leanCloud';
-import SignUpForm from './SignUpForm'
+import SignUpForm from './SignUpForm';
+import SignInForm from './SignInForm'
 
 
 export default class UserDialog extends Component{
@@ -66,24 +67,6 @@ export default class UserDialog extends Component{
    this.setState(stateCopy)
  }
  render(){
-    let signInForm = (
-      <form className="signIn" onSubmit={this.signIn.bind(this)}> {/* 登录*/}
-       <div className="row">
-         <label>用户名</label>
-         <input type="text" value={this.state.formData.username}
-           onChange={this.changeFormData.bind(this, 'username')}/>
-       </div>
-       <div className="row">
-         <label>密码</label>
-         <input type="password" value={this.state.formData.password}
-           onChange={this.changeFormData.bind(this, 'password')}/>
-       </div>
-       <div className="row actions">
-         <button type="submit">登录</button>
-         <a href="#" onClick={this.showForgotPassword.bind(this)}>忘记密码了？</a>
-        </div>
-      </form>
-    ) 
     let signInOrSignUp = (
         <div className="signInOrSignUp">
           <nav>
@@ -102,9 +85,16 @@ export default class UserDialog extends Component{
           {this.state.selected === 'signUp' ?
             <SignUpForm formData={this.state.formData}
               onSubmit={this.signUp.bind(this)}
-              onChange={this.changeFormData.bind(this)}/>
+              onChange={this.changeFormData.bind(this)}
+              />
+              : null}
+            {this.state.selected === 'signIn' ?
+              <SignInForm formData={this.state.formData}
+                onChange={this.changeFormData.bind(this)}
+                onSubmit={this.signIn.bind(this)}
+                onForgotPassword={this.showForgotPassword.bind(this)}
+              />
             : null}
-            {this.state.selected === 'signIn' ? signInForm : null}
           </div>
         </div>
       )
